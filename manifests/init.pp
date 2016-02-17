5defc5b86f867586ade1fe2ba6f3ec632f80762f
sontru <sontru@googlemail.com>
2016-02-17 13:17:17 +0000

    Added the "

--- manifests/init.pp
+++ manifests/init.pp
@@ -438,7 +438,7 @@ class authconfig (
       $pass_flags            = "${md5_flg} ${passalgo_val} ${shadow_flg}"
       $authconfig_flags      = "${ldap_flags} ${nis_flags} ${pass_flags} ${krb5_flags} ${winbind_flags} ${extra_flags} ${cache_flg} ${mkhomedir_flg} ${sssd_flg} ${sssdauth_flg} ${locauthorize_flg} ${sysnetauth_flg}"
       $authconfig_update_cmd = "authconfig ${authconfig_flags} --updateall"
-      $authconfig_test_flags = ${ldap_flags} ${nis_flags} ${pass_flags} ${krb5_flags_test} ${winbind_flags} ${extra_flags} ${cache_flg} ${mkhomedir_flg} ${sssd_flg} ${sssdauth_flg} ${locauthorize_flg} ${sysnetauth_flg}"
+      $authconfig_test_flags = "${ldap_flags} ${nis_flags} ${pass_flags} ${krb5_flags_test} ${winbind_flags} ${extra_flags} ${cache_flg} ${mkhomedir_flg} ${sssd_flg} ${sssdauth_flg} ${locauthorize_flg} ${sysnetauth_flg}"
       $authconfig_test_cmd   = "authconfig ${authconfig_test_flags} --test"
       $exec_check_cmd        = "/usr/bin/test \"`${authconfig_test_cmd}`\" = \"`authconfig --test`\""
 
